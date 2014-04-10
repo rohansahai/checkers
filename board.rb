f3666c4e481d568bec5db57d4b0d3a732811fdcd
Rohan Sahai <rsahai91@gmail.com>
2014-04-10 14:04:28 -0700

    Moving pieces into valid positions, implementing initial king functionality

---
+++ board.rb
@@ -0,0 +1,94 @@
+require "./piece.rb"
+require 'colorize'
+class Board
+  attr_accessor :spaces, :pieces
+  
+  OPP_COLOR = {:red => :black, :black => :red}
+  
+  def initialize(options = {})
+    @spaces = Array.new(8) { Array.new(8) }
+    @pieces = {:red => [], :black => []}
+    populate_board unless options[:empty]
+  end
+  
+  def populate_board
+    rows = [0, 1, 2, 5, 6, 7]
+    
+    rows.each do |row|
+      0.upto(7) do |col|
+        if row.even? && col.even?
+          @spaces[row][col] = Piece.new([col, row], self, :black) if row < 3
+          @spaces[row][col] = Piece.new([col, row], self, :red) if row > 3
+        elsif row.odd? && col.odd?
+          @spaces[row][col] = Piece.new([col, row], self, :black) if row < 3
+          @spaces[row][col] = Piece.new([col, row], self, :red) if row > 3
+        end
+      end
+    end
+    nil
+  end
+  
+  def is_empty?(pos)
+    @spaces[pos.last][pos.first] == nil ? true : false
+  end
+  
+  def valid_pos?(pos)
+    on_board = (0..7).to_a
+    if on_board.include?(pos.first) && on_board.include?(pos.last)
+      return true
+    end
+    false
+  end
+  
+  def [](pos)
+      raise "invalid pos" unless valid_pos?(pos)
+      i, j = pos
+      @spaces[j][i]
+    end
+  
+  def []=(pos, piece)
+    @spaces[pos.last][pos.first] = piece
+  end
+  
+  def render
+    print "  "
+    puts (0..7).to_a.join(" ")
+    @spaces.each_with_index do |row, i|
+      print "#{i} "
+      row.each do |col|
+        if col == nil
+          print "* ".yellow
+        elsif col.king == true
+          print "K "
+        elsif col.color == :black
+          print "B ".blue
+        elsif col.color == :red
+          print "R ".red
+        end
+      end
+      puts ""
+    end
+  end
+  
+end
+
+new_board = Board.new
+new_board[[0,2]].perform_slide([1,3])
+new_board[[3,5]].perform_slide([2,4])
+new_board[[1,3]].perform_jump([3,5])
+new_board[[4,6]].perform_jump([2,4])
+new_board[[2,4]].king = true
+new_board[[2,4]].perform_slide([4,5])
+#new_board[[2,4]].perform_jump([1,3])
+
+new_board.render
+
+# new_board.spaces.each do |spaces|
+#   spaces.each do |spot|
+#     if spot != nil
+#       p spot.position
+#     else
+#       p nil
+#     end
+#   end
+# end
---
+++ piece.rb
@@ -0,0 +1,97 @@
+class Piece
+  attr_accessor :position, :board, :color, :king
+  
+  def initialize(position, board, color, king = false)
+    @position = position
+    @board = board
+    @color = color
+    @king = king
+  end
+  
+  def forward_dir
+    (color == :red) ? -1 : 1
+  end
+  
+  def find_valid_slides
+    i, j = self.position
+    valid_slides = [[i + 1, j + forward_dir], [i - 1, j + forward_dir]]
+    
+    if self.king
+      valid_slides += [[i + 1, j - forward_dir]] + [[i - 1, j - forward_dir]]
+    end
+
+    moves = valid_slides.select do |slide_pos|
+      board.valid_pos?(slide_pos) && board.is_empty?(slide_pos)
+    end
+    moves
+  end
+  
+  def find_valid_jumps
+    moves = []
+    enemy_pos = [] #this one is to return
+    i, j = self.position
+    valid_jumps = [[i + 2, j + forward_dir*2], [i - 2, j + forward_dir*2]]
+    enemy_positions = [[i + 1, j + forward_dir], [i - 1, j + forward_dir]]
+    
+    if self.king
+      valid_slides += [i + 1, j - forward_dir*2] + [i - 1, j - forward_dir*2]
+    end
+    
+    valid_jumps.each_with_index do |jump_pos, idx|
+      if board.valid_pos?(jump_pos) && is_enemy?(enemy_positions[idx])
+        moves << jump_pos
+        enemy_pos << enemy_positions[idx]
+      end
+    end
+    [moves, enemy_pos]
+  end
+  
+  def perform_slide(pos)
+    if find_valid_slides.include?(pos)
+      move_piece(pos)
+    else
+      puts "Invalid Move" #change this to raising exception
+    end
+  end
+  
+  def perform_jump(pos)
+    valid_jumps, enemy_pos = find_valid_jumps
+    if valid_jumps.include?(pos)
+      idx = valid_jumps.index(pos)
+      move_piece(pos, enemy_pos[idx])
+    else
+      puts "Invalid Move" #change this to raising exception
+    end
+  end
+  
+  def is_enemy?(pos)
+    if !@board.is_empty?(pos) && @board[pos].color != self.color
+      return true
+    end
+    false
+  end
+  
+  #Delete when jump?
+  def move_piece(pos, enemy_pos = nil)
+    @board[position] = nil  #set old spot to empty
+    @position[0] = pos.first  #update pieces location
+    @position[1] = pos.last
+    @board[pos] = self  #update board with new piece
+    
+    if enemy_pos !=nil
+      @board[enemy_pos] = nil
+    end
+    maybe_promote(pos)
+  end
+  
+  def maybe_promote(pos)
+    (self.color == :black) ? king_row = 7 : king_row = 0
+    if pos.last == king_row
+      self.king = true
+    end
+  end
+  
+end
+
+# x = Piece.new([5,5], nil, :black) # 4,6  6,6   - 3, 7  7,7
+# p x.valid_slides
